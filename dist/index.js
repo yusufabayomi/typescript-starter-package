d6e48a750976db4ffa074782acc21dc5455da8d5
yusufabayomi <yusufabayomio@gmail.com>
2023-03-29 15:26:18 +0100

    initialise package

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = function () {
+exports.helloWorld = void 0;
+var helloWorld = function () {
     return 'Howdy!';
 };
+exports.helloWorld = helloWorld;
 //# sourceMappingURL=index.js.map
